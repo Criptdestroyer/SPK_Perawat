e1ac4f2fb7ec90bc63719e89be57280a9aa55e4b
Criptdestroyer <ahmademir1201@gmail.com>
2019-03-11 18:17:18 +0700

    template function

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -14,5 +14,13 @@
             // $this->load->library('lib_log');
             date_default_timezone_set("Asia/Jakarta");
         }
+
+        protected function template($data, $module = '')
+        {
+            $data['global_title'] = $this->title;
+            $data['module'] = $module;
+            if(strlen($module)>0) return $this->load->view($module.'/includes/layout',$data);
+            return $this->load->view('includes/layout',$data);
+        }
     }
 ?>
